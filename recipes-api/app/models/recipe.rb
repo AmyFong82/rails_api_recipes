1bf640770596a5f90dfc34838c952bf4ffee97ea
AmyFong82 <amyhfong@gmail.com>
2022-03-03 14:47:08 -0500

    added validations to the recipe model.

--- recipes-api/app/models/recipe.rb
+++ recipes-api/app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ApplicationRecord
+  # turn the text to array format
   serialize :ingredients, Array
   serialize :instructions, Array
+
+  # validations
+  validates_presence_of :name, :ingredients, :instructions
 end
