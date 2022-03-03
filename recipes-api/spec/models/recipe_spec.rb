977b9447bab1e380becf6e371264f4dda3af225a
AmyFong82 <amyhfong@gmail.com>
2022-03-03 11:08:31 -0500

    wrote the Validation tests for Recipe model.

--- recipes-api/spec/models/recipe_spec.rb
+++ recipes-api/spec/models/recipe_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Validation tests
+  # ensure name, ingredients and instructions are present before saving
+  it { should validate_presence_of(:name)}
+  it { should validate_presence_of(:ingredients)}
+  it { should validate_presence_of(:instructions)}
 end
