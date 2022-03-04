64b85df766b173dc99a3375c574eafcae123703e
AmyFong82 <amyhfong@gmail.com>
2022-03-03 21:58:49 -0500

    added unique for the Faker Lorem.

--- recipes-api/spec/factories/recipes.rb
+++ recipes-api/spec/factories/recipes.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :recipe do
     # Because the name is a camel case string with no space,
     # I can only use Faker::Lorem.word for testing data.
-    name { Faker::Lorem.word }
+    name { Faker::Lorem.unique.word }
     ingredients { [Faker::Food.ingredient, Faker::Food.ingredient] }
     instructions { Faker::Lorem.sentences }
   end
